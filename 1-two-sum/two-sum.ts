9c3458803ba30aa4d89793ecb94dcfae449123c5
kruzaandrei <58821761+kruzaandrei@users.noreply.github.com>
2026-01-21 17:31:17 +0200

    Time: 5 ms (44.92%) | Memory: 57.2 MB (45.09%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,16 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
     const numberToIndex = new Map();
-
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
-
         if (numberToIndex.has(complement)) {
             return [numberToIndex.get(complement), i];
         }
-
         numberToIndex.set(nums[i], i);
     }
-
-    return [];
 };
 
