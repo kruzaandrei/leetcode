48f8bacd30c46b45fd5220f3a26c810f5700df80
kruzaandrei <58821761+kruzaandrei@users.noreply.github.com>
2026-01-21 17:15:56 +0200

    Time: 0 ms (100.00%) | Memory: 59.3 MB (50.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -1,7 +1,28 @@
 function isPalindrome(s: string): boolean {
-    const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    
-    const reversed = cleaned.split('').reverse().join('');
-    
-    return cleaned === reversed;
+    let l = 0;
+    let r = (s.length - 1);
+
+    while (l < r) {
+        while (l < r && !isAlphaNumeric(s[l])) {  // Note 1, 2
+            l += 1;
+        }
+        while (l < r && !isAlphaNumeric(s[r])) {
+            r -= 1;
+        }
+
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+            return false
+        }
+
+        l += 1;
+        r -= 1;
+    }
+    return true;
 };
+
+function isAlphaNumeric(char: string): boolean {
+    const code = char.charCodeAt(0);
+    return (code >= 48 && code <= 57) ||   // 0-9
+        (code >= 65 && code <= 90) ||   // A-Z
+        (code >= 97 && code <= 122);    // a-z
+}
